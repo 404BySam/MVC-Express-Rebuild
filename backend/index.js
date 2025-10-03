401968a241ea4f7c02d39ece27a0c57aa5c4ab03
404BySam <devbysam404@gmail.com>
2025-10-03 14:53:53 +0200

    add connection DB

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const mysql = require("mysql2/promise");
 const app = require("./src/app");
 
 const port = parseInt(process.env.APP_PORT ?? "5000", 10);
@@ -10,3 +11,18 @@ app.listen(port, (err) => {
     console.log(`serveur prêt sur http://localhost:${port}`);
   }
 });
+
+(async () => {
+  try {
+    const db = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+
+    console.log("✅ Connexion MySQL réussie !");
+  } catch (err) {
+    console.error("❌ Erreur de connexion :", err.message);
+  }
+})();
